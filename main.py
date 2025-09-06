8f5e47793af0278d739b829b6b4c74b359b97ef4
gunaykarim9-arch <gunay.karim9@gmail.com>
2025-09-06 10:18:27 +0000

    Add FCC demographic analyzer + dataset (skipinitialspace)

---
+++ main.py
@@ -0,0 +1,5 @@
+from demographic_data_analyzer import calculate_demographic_data
+
+if __name__ == '__main__':
+    # Make sure 'adult.data.csv' is in the same folder.
+    calculate_demographic_data(print_data=True)
